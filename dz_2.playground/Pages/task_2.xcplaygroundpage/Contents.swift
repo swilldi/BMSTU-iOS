ea10743da29ca2c10b38146be648dc170d299ae5
Dmitriy Dudyrev <dmitriy@MacBook-Pro--Dmitriy.local>
2025-02-27 21:34:29 +0300

    complete task_2

--- dz_2.playground/Pages/task_2.xcplaygroundpage/Contents.swift
+++ dz_2.playground/Pages/task_2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,36 @@
-//: [Previous](@previous)
+/*
+ Дана строка, состоящая только из круглых скобок. Проверить является ли последовательность скобок корректной и вывести результат в консоль.
+ let string1 = "(())"
+ Output: Корректная
+
+ let string2 = "))(("
+ Output: Некорректная
+
+ let string1 = "()()()"
+ Output: Корректная
+ */
 
 import Foundation
 
-var greeting = "Hello, playground"
+let string = "(())))"
+var count_open: Int = 0
+var result = "Корректная"
 
-//: [Next](@next)
+for symbol in string {
+    if symbol == "(" {
+        count_open += 1
+    } else {
+        count_open -= 1
+    }
+    
+    if count_open < 0 {
+        result = "Некорректная"
+        break
+    }
+}
+
+if count_open != 0 {
+    result = "Некорректная"
+}
+
+print(result)
