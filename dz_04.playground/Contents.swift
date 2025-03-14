5444a66ce471cd50aa318a2cbb48af5b0ca50942
Dmitriy Dudyrev <dmitriy@MacBook-Pro--Dmitriy.local>
2025-03-14 22:15:59 +0300

    complete code

--- dz_04.playground/Contents.swift
+++ dz_04.playground/Contents.swift
@@ -1,12 +1,55 @@
-class GameCharacter {
+// Протокол, чтобы экземпляр мог лечиться
+protocol Healable {
+    var health: Int { get set }
+    func heal(amount: Int)
+}
+
+
+// Протокол, чтобы экземпляр мог летать
+protocol Flyable {
+    var flightSpeed: Int { get set }
+    func fly()
+}
+
+//
+protocol Item {
+    var label: String { get }
+    var type: ItemType { get }
+    var action: (GameCharacter) -> () { get }
+    func use(to character: GameCharacter)
+}
+
+
+enum ItemType: String {
+    case healingPothion = "Целебное зелье"
+    case levelUppingPotion = "Зелье повышения уровня"
+
+    var action: (GameCharacter) -> () {
+        switch self {
+        case .healingPothion:
+            { character in character.heal(amount: 20) }
+        case .levelUppingPotion:
+            { character in character.levelUp() }
+        }
+    }
+}
+
+
+class GameCharacter: Healable {
+    // начальные значения характиристик
     let defaultHealth = 100
     let defaultLevel = 1
+    
+    // коэффициенты повышение характиристик
     let healthCoefficient = 10
     
+    // объявляем поля класса
     let name: String
     var maxHealth: Int
     var health: Int
     var level: Int
+    var inventory = [String:Int]()
+    var dustinessInventory = 10
     
     
     init(name: String, health: Int, level: Int) {
@@ -16,6 +59,7 @@ class GameCharacter {
         self.health = maxHealth
     }
     
+    // получение урона
     func takeDamage(amount damage: Int) {
         health -= damage
         if health < 0 {
@@ -23,6 +67,7 @@ class GameCharacter {
         }
     }
     
+    // востановление здоровья
     func heal(amount points: Int) {
         health += points
         if health > maxHealth {
@@ -30,15 +75,35 @@ class GameCharacter {
         }
     }
     
+    // повышение уровня и характеристик
     func levelUp() {
-        // повышаем характеристики
         level += 1
         maxHealth += healthCoefficient
         health = maxHealth
     }
+    
+    func takeItem(item: Item) {
+        inventory[item.label] = 1 + (inventory[item.label] ?? 0)
+    }
+    
+    func use(item: Item) {
+        if inventory[item.label] ?? 0 > 0 {
+            inventory[item.label]! -= 1
+            item.action(self)
+        }
+    }
 }
 
 
+// Добавляем раширение для класса GameCharacter
+extension GameCharacter {
+    var isAlive: Bool { health > 0}
+    func printCharacterInfo() {
+        print("Name: \(name)\nLevel: \(level)\nHealth: \(health)")
+    }
+}
+
+    
 let player = GameCharacter(name: "Player_1", health: 100, level: -1)
 player.maxHealth
 player.levelUp()
@@ -46,11 +111,15 @@ player.maxHealth
 
 
 class Warrior: GameCharacter {
+    // начальные значения характиристик
     let defaultArmor = 2
     let defaultPower = 10
+    
+    // коэффициенты повышение характиристик
     let armorCoefficient = 1
     let powerCoefficient = 1
     
+    // объявляем поля класса
     var power: Int
     var armor: Int
     
@@ -60,30 +129,97 @@ class Warrior: GameCharacter {
         self.power = power <= 0 ? defaultPower : power
         super.init(name: name, health: health, level: level)
         
+        // повышение характеристик в соответсвии с уровнем персонажа
         self.armor += armorCoefficient * (level - defaultLevel) / 2
-        self.power += powerCoefficient * (level - defaultLevel) / 2
+        self.power += powerCoefficient * (level - defaultLevel)
     }
     
+    // атака другого экземпляра класса GameCharacter
     func attack(target: GameCharacter) {
         target.takeDamage(amount: power)
     }
     
+    // получение уроная с учетом брони
     override func takeDamage(amount damage: Int) {
         if damage > armor {
             super.takeDamage(amount: damage - armor)
         }
     }
     
+    // повышение уровня и характеристик
     override func levelUp() {
-        // повышаем характеристики
         super.levelUp()
-        armor += armorCoefficient
         power += powerCoefficient
+        if level % 2 != 0 {
+            armor += armorCoefficient
+        }
     }
     
 }
 
-let warrior_1 = Warrior(name: "warrior_1", health: 100, armor: 2, power: 10, level: 10)
 
+class Mage: GameCharacter, Flyable {
+    // начальные значения характиристик
+    let defaultPhysicPower = 5
+    let defaultMagicPower = 10
+    
+    // коэффициенты повышение характиристик
+    let physicPowerCoefficient = 1
+    let magicPowerCoefficient = 1
+    
+    // объявляем поля класса
+    var magicPower: Int
+    var physicPower: Int
+    var flightSpeed = 10
+    
+    init(name: String, health: Int, magicPower: Int, physicPower: Int, level: Int) {
+        self.magicPower = magicPower <= 0 ? magicPower : defaultMagicPower
+        self.physicPower = physicPower <= 0 ? physicPower : defaultPhysicPower
+        super.init(name: name, health: health, level: level)
+        
+        self.magicPower += magicPowerCoefficient * (level - defaultLevel)
+        self.physicPower += physicPowerCoefficient * (level - defaultLevel)
+    }
+    
+    // атака другого экземпляра класса GameCharacter
+    func attack(target: GameCharacter) {
+        target.takeDamage(amount: physicPower)
+    }
+    
+    // усиленная атака другого экземпляра класса GameCharacter
+    func magicAttack(target: GameCharacter) {
+        target.takeDamage(amount: physicPower + magicPower)
+    }
+    
+    func fly() {
+        print("I can fly \(flightSpeed) m/h")
+    }
+    
+    // повышение уровня и характеристик
+    override func levelUp() {
+        super.levelUp()
+        magicPower += magicPowerCoefficient
+        if level % 2 != 0 {
+            physicPower += physicPowerCoefficient
+        }
+    }
+}
+
+
+struct Potion: Item {
+    let type: ItemType
+    var label: String
+    let action: (GameCharacter) -> ()
+    
+    init(type: ItemType) {
+        self.type = type
+        self.label = type.rawValue
+        self.action = type.action
+    }
+    
+    func use(to character: GameCharacter) {
+        action(character)
+    }
+}
 
 
