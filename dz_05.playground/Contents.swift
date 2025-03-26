c0e182437e5439c01bc99b9c379807cc44fb4c07
Dmitriy Dudyrev <dmitriy@MacBook-Pro--Dmitriy.local>
2025-03-26 18:25:16 +0300

    make edits

--- dz_05.playground/Contents.swift
+++ dz_05.playground/Contents.swift
@@ -1,82 +1,16 @@
+import Foundation
+
 // MARK: ЗАДАНИЕ 5
 
 // MARK: Классы с циклической ссылкой
-//class Person {
-//    let name: String
-//    var car: Car?
-//
-//    init(name: String) {
-//        print("Создан \(name)")
-//        self.name = name
-//    }
-//
-//    deinit {
-//        print("Уничтожен \(name)")
-//    }
-//}
-//
-//class Car {
-//    var color: String
-//    var owner: Person?
-//
-//    init(color: String, owner: Person?) {
-//        print("Создана машина \(color)")
-//        self.color = color
-//        self.owner = owner
-//    }
-//
-//    deinit {
-//        print("Уничтожена машина \(color)")
-//    }
-//}
-//
-//
-//var person1: Person? = Person(name: "Jack")
-//var car1: Car? = Car(color: "Red", owner: person1)
-//person1!.car = car1
-//// удалаяем экземпляр person1
-//person1 = nil
-//// пытаемся обратиться к person через car, выводит nil
-//print("car1?.owner: \(car1?.owner)")
-//// удаляем car1
-//car1 = nil
-//// провереям, что нет возможности обратиться
-//print("car1?.owner: \(car1?.owner)")
-//print("person1?.car: \(person1?.car)")
-//
-//// тоже самое но начнем с жкземпляра car1
-//var person2: Person? = Person(name: "Ann")
-//var car2: Car? = Car(color: "Green", owner: person2)
-//person2!.car = car2
-//// удалаяем экземпляр car2
-//car2 = nil
-//// пытаемся обратиться к person через car, выводит nil
-//print("person2?.car: \(person2?.car)")
-////удаляем perosn2
-//person2 = nil
-//// провереям, что нет возможности обратиться
-//print("person2?.car: \(person2?.car)")
-//print("car2?.owner: \(car2?.owner)")
-
-/*
- MARK: Мини вывод
- Ну на данный момент я не нашел функции, которая выводи
- кол-во ссылок на объект, поэтому хотя бы тексто напишу.
- В данном случае 4 раза выполняется print() из инициализаторов,
- но ни одного print'а из деиницилизаторов => ни один из экземпляров не
- удалаяется полностью, а висит в памяти
- */
-
-
-
-//MARK: Без циклического замыкания
 class Person {
     let name: String
-    weak var car: Car?
+    var car: Car?
 
-    init(name: String) {
+    init(name: String, car: Car?) {
         print("Создан \(name)")
         self.name = name
+        self.car = car
     }
 
     deinit {
@@ -86,7 +20,7 @@ class Person {
 
 class Car {
     var color: String
-    weak var owner: Person?
+    var owner: Person?
 
     init(color: String, owner: Person?) {
         print("Создана машина \(color)")
@@ -100,9 +34,11 @@ class Car {
 }
 
 
-var person1: Person? = Person(name: "Jack")
+var person1: Person? = Person(name: "Jack", car: nil)
 var car1: Car? = Car(color: "Red", owner: person1)
 person1!.car = car1
+print("Кол-во ссылок на car1: \(CFGetRetainCount(car1))")
+print("Кол-во ссылок на person1: \(CFGetRetainCount(person1))")
 // удалаяем экземпляр person1
 person1 = nil
 // пытаемся обратиться к person через car, выводит nil
@@ -113,10 +49,14 @@ car1 = nil
 print("car1?.owner: \(car1?.owner)")
 print("person1?.car: \(person1?.car)")
 
+print("---")
+
 // тоже самое но начнем с жкземпляра car1
-var person2: Person? = Person(name: "Ann")
+var person2: Person? = Person(name: "Ann", car: nil)
 var car2: Car? = Car(color: "Green", owner: person2)
 person2!.car = car2
+print("Кол-во ссылок на car2: \(CFGetRetainCount(car2))")
+print("Кол-во ссылок на person2: \(CFGetRetainCount(person2))")
 // удалаяем экземпляр car2
 car2 = nil
 // пытаемся обратиться к person через car, выводит nil
@@ -126,6 +66,81 @@ person2 = nil
 // провереям, что нет возможности обратиться
 print("person2?.car: \(person2?.car)")
 print("car2?.owner: \(car2?.owner)")
+
+/*
+ MARK: Мини вывод
+ Ну на данный момент я не нашел функции, которая выводи
+ кол-во ссылок на объект, поэтому хотя бы тексто напишу.
+ В данном случае 4 раза выполняется print() из инициализаторов,
+ но ни одного print'а из деиницилизаторов => ни один из экземпляров не
+ удалаяется полностью, а висит в памяти
+ */
+
+
+
+//MARK: Без циклического замыкания
+//class Person {
+//    let name: String
+//    weak var car: Car?
+//
+//    init(name: String, car: Car?) {
+//        print("Создан \(name)")
+//        self.name = name
+//    }
+//
+//    deinit {
+//        print("Уничтожен \(name)")
+//    }
+//}
+//
+//class Car {
+//    var color: String
+//    weak var owner: Person?
+//
+//    init(color: String, owner: Person?) {
+//        print("Создана машина \(color)")
+//        self.color = color
+//        self.owner = owner
+//    }
+//
+//    deinit {
+//        print("Уничтожена машина \(color)")
+//    }
+//}
+//
+//
+//var person1: Person? = Person(name: "Jack", car: nil)
+//var car1: Car? = Car(color: "Red", owner: person1)
+//person1!.car = car1
+//print("Кол-во ссылок на car1: \(CFGetRetainCount(car1))")
+//print("Кол-во ссылок на person1: \(CFGetRetainCount(person1))")
+//// удалаяем экземпляр person1
+//person1 = nil
+//// пытаемся обратиться к person через car, выводит nil
+//print("car1?.owner: \(car1?.owner)")
+//// удаляем car1
+//car1 = nil
+//// провереям, что нет возможности обратиться
+//print("car1?.owner: \(car1?.owner)")
+//print("person1?.car: \(person1?.car)")
+//
+//print("---")
+//
+//// тоже самое но начнем с жкземпляра car1
+//var person2: Person? = Person(name: "Ann", car: nil)
+//var car2: Car? = Car(color: "Green", owner: person2)
+//person2!.car = car2
+//print("Кол-во ссылок на car2: \(CFGetRetainCount(car2))")
+//print("Кол-во ссылок на person2: \(CFGetRetainCount(person2))")
+//// удалаяем экземпляр car2
+//car2 = nil
+//// пытаемся обратиться к person через car, выводит nil
+//print("person2?.car: \(person2?.car)")
+////удаляем perosn2
+//person2 = nil
+//// провереям, что нет возможности обратиться
+//print("person2?.car: \(person2?.car)")
+//print("car2?.owner: \(car2?.owner)")
 
 /*
  MARK: Мини вывод
