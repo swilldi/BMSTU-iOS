80c712058f1c415f1d10eb965cb5114865692bf7
Dmitriy Dudyrev <dmitriy@MacBook-Pro--Dmitriy.local>
2025-03-26 17:55:17 +0300

    fix bugs

--- dz_05.playground/Contents.swift
+++ dz_05.playground/Contents.swift
@@ -10,8 +10,7 @@
 //        self.name = name
 //    }
 //
-//    deinit
-//    {
+//    deinit {
 //        print("Уничтожен \(name)")
 //    }
 //}
@@ -20,79 +19,128 @@
 //    var color: String
 //    var owner: Person?
 //
-//    init(color: String, owner: Person?)
-//    {
+//    init(color: String, owner: Person?) {
 //        print("Создана машина \(color)")
 //        self.color = color
 //        self.owner = owner
 //    }
 //
-//    deinit
-//    {
+//    deinit {
 //        print("Уничтожена машина \(color)")
 //    }
 //}
 //
-//if true {
-//    let person = Person(name: "Jack")
-//    let car: Car? = Car(color: "Red", owner: person)
-//    person.car = car
 //
-//    print()
-//}
+//var person1: Person? = Person(name: "Jack")
+//var car1: Car? = Car(color: "Red", owner: person1)
+//person1!.car = car1
+//// удалаяем экземпляр person1
+//person1 = nil
+//// пытаемся обратиться к person через car, выводит nil
+//print("car1?.owner: \(car1?.owner)")
+//// удаляем car1
+//car1 = nil
+//// провереям, что нет возможности обратиться
+//print("car1?.owner: \(car1?.owner)")
+//print("person1?.car: \(person1?.car)")
 //
-//print()
+//// тоже самое но начнем с жкземпляра car1
+//var person2: Person? = Person(name: "Ann")
+//var car2: Car? = Car(color: "Green", owner: person2)
+//person2!.car = car2
+//// удалаяем экземпляр car2
+//car2 = nil
+//// пытаемся обратиться к person через car, выводит nil
+//print("person2?.car: \(person2?.car)")
+////удаляем perosn2
+//person2 = nil
+//// провереям, что нет возможности обратиться
+//print("person2?.car: \(person2?.car)")
+//print("car2?.owner: \(car2?.owner)")
+
+/*
+ MARK: Мини вывод
+ Ну на данный момент я не нашел функции, которая выводи
+ кол-во ссылок на объект, поэтому хотя бы тексто напишу.
+ В данном случае 4 раза выполняется print() из инициализаторов,
+ но ни одного print'а из деиницилизаторов => ни один из экземпляров не
+ удалаяется полностью, а висит в памяти
+ */
+
 
 
 //MARK: Без циклического замыкания
-//class Person {
-//    let name: String
-//    weak var car: Car?
-//
-//    init(name: String) {
-//        print("Создан \(name)")
-//        self.name = name
-//    }
-//
-//    deinit
-//    {
-//        print("Уничтожен \(name)")
-//    }
-//}
-//
-//class Car {
-//    var color: String
-//    var owner: Person?
-//
-//    init(color: String, owner: Person?)
-//    {
-//        print("Создана машина \(color)")
-//        self.color = color
-//        self.owner = owner
-//    }
-//
-//    deinit
-//    {
-//        print("Уничтожена машина \(color)")
-//    }
-//}
-//
-//if true {
-//    let person = Person(name: "Jack")
-//    let car: Car? = Car(color: "Red", owner: person)
-//    person.car = car
-//}
-//print()
+class Person {
+    let name: String
+    weak var car: Car?
+
+    init(name: String) {
+        print("Создан \(name)")
+        self.name = name
+    }
+
+    deinit {
+        print("Уничтожен \(name)")
+    }
+}
+
+class Car {
+    var color: String
+    weak var owner: Person?
+
+    init(color: String, owner: Person?) {
+        print("Создана машина \(color)")
+        self.color = color
+        self.owner = owner
+    }
+
+    deinit {
+        print("Уничтожена машина \(color)")
+    }
+}
 
 
+var person1: Person? = Person(name: "Jack")
+var car1: Car? = Car(color: "Red", owner: person1)
+person1!.car = car1
+// удалаяем экземпляр person1
+person1 = nil
+// пытаемся обратиться к person через car, выводит nil
+print("car1?.owner: \(car1?.owner)")
+// удаляем car1
+car1 = nil
+// провереям, что нет возможности обратиться
+print("car1?.owner: \(car1?.owner)")
+print("person1?.car: \(person1?.car)")
 
-// MARK: ЗАДАНИЕ 6
+// тоже самое но начнем с жкземпляра car1
+var person2: Person? = Person(name: "Ann")
+var car2: Car? = Car(color: "Green", owner: person2)
+person2!.car = car2
+// удалаяем экземпляр car2
+car2 = nil
+// пытаемся обратиться к person через car, выводит nil
+print("person2?.car: \(person2?.car)")
+//удаляем perosn2
+person2 = nil
+// провереям, что нет возможности обратиться
+print("person2?.car: \(person2?.car)")
+print("car2?.owner: \(car2?.owner)")
 
+/*
+ MARK: Мини вывод
+ при присовении экземплярую nil выполняется его деинициализтор
+ => при присовении экземпляру nil он уничтожается полностью, не висит в памяти
+ и не появляется зацикливания ссылок
+ */
+
+
+//// MARK: ЗАДАНИЕ 6
+//
 //class Animal {
 //    let name: String
 //
-//    init(name: String)
-//    {
+//    init(name: String) {
 //        self.name = name
 //    }
 //
@@ -103,8 +151,7 @@
 //class Dog: Animal {
 //    let voice = "Woaf!"
 //
-//    override func speak()
-//    {
+//    override func speak() {
 //        print(voice)
 //    }
 //}
@@ -112,14 +159,13 @@
 //class Cat: Animal {
 //    let voice = "Meow!"
 //
-//    override func speak()
-//    {
+//    override func speak() {
 //        print(voice)
 //    }
 //}
 //
-//let dog_1 = Dog(name: "Astin"), dog_2 = Dog(name: "Crocky"), cat_1 = Cat(name: "Tom")
-//let animals: Array<Animal> = [dog_1, cat_1, dog_2]
+//let dog1 = Dog(name: "Astin"), dog2 = Dog(name: "Crocky"), cat1 = Cat(name: "Tom")
+//let animals: Array<Animal> = [dog1, cat1, dog2]
 //for animal in animals {
 //    animal.speak()
 //}
