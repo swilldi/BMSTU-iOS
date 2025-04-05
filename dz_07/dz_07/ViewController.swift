2989a7aeb1849477680985a05363f0a6a667c739
Dmitriy Dudyrev <dmitriy@MacBook-Pro--Dmitriy.local>
2025-04-05 18:48:42 +0300

    add comments

--- dz_07/dz_07/ViewController.swift
+++ dz_07/dz_07/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController {
         setupConstrains()
     }
     
+    // MARK: Создание изображения
     private func setupImage() {
         let image = UIImage(named: item.title)
         
@@ -45,6 +46,7 @@ class ViewController: UIViewController {
         itemImage = imageView
     }
     
+    // MARK: Создание названия компании
     private func setupCompany() {
         let factory = UILabel()
         factory.font = .boldSystemFont(ofSize: 18)
@@ -57,6 +59,7 @@ class ViewController: UIViewController {
         companyLabel = factory
     }
     
+    // MARK: Создание названия предмета
     private func setupTitile() {
         let title = UILabel()
         title.font = .boldSystemFont(ofSize: 20)
@@ -69,6 +72,7 @@ class ViewController: UIViewController {
         titleLabel = title
     }
     
+    // MARK: Создание цены
     private func setupPrise() {
         let prise = UILabel()
         prise.font = .boldSystemFont(ofSize: 23)
@@ -81,6 +85,7 @@ class ViewController: UIViewController {
         priseLabel = prise
     }
     
+    // MARK: Создание кнопки перехода в следующему товару
     private func setupButton() {
         let button = UIButton()
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
@@ -97,6 +102,7 @@ class ViewController: UIViewController {
         nextItemButton = button
     }
     
+    // MARK: Дейстия при нажатии кнопки
     @objc private func didTapButton() {
         // Вот тут вопрос для чего тут требуется вставка "as! any Item"
         item = item.next() as! any Item
@@ -107,7 +113,7 @@ class ViewController: UIViewController {
         priseLabel?.text = item.prise
     }
     
-    
+    // MARK: Создание относительного расположения элементов
     private func setupConstrains() {
         guard let itemImage, let companyLabel, let titleLabel, let priseLabel, let nextItemButton else { return }
     
@@ -119,23 +125,25 @@ class ViewController: UIViewController {
         nextItemButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
+            // изображение
             itemImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             itemImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             itemImage.widthAnchor.constraint(equalTo: view.widthAnchor),
             itemImage.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
             
-            
+            // название компании
             companyLabel.topAnchor.constraint(equalTo: itemImage.bottomAnchor),
             companyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             
-            
+            // название предмета
             titleLabel.topAnchor.constraint(equalTo: companyLabel.bottomAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             
+            // цена предмета
             priseLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
             priseLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             
-            
+            // кнопка перехода к следующему предмету
             nextItemButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
             nextItemButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             nextItemButton.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4),
