4748b1069749e748a10172086219b73a8ab518b4
Dmitriy <dimitruo@yandex.ru>
2025-04-11 11:44:31 +0300

    add dz_08

---
+++ dz_08/dz_08/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dz_08
+//
+//  Created by Dmitriy Dudyrev on 11.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
